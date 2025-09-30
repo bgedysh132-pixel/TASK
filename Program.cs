3e78cc6c35da5ad4aa8fa5477bce73405253c93b
Boris Gedysh <Boris Gedysh@DESKTOP-IF9AAV7>
2025-09-30 21:27:33 +0500

    task01

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Автор: Александр Сергеевич Пушкин\n");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Название: Я помню чудное мгновенье\n");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Я помню чудное мгновенье:");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Передо мной явилась ты,");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Как мимолётное виденье,");
+        Console.ForegroundColor = ConsoleColor.Purple;
+        Console.WriteLine("Как гений чистой красоты.");
+
+        Console.ResetColor();
+
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
