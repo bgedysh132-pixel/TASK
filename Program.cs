3adb0581da4488b58af047f9ae5bf35b757ab27c
Boris Gedysh <Boris Gedysh@DESKTOP-IF9AAV7>
2025-10-08 23:24:16 +0500

    task04

--- Program.cs
+++ Program.cs
@@ -4,32 +4,26 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Программа для вычисления функции y = √(sinx - sin2x - sin3x - sin4x)");
+
         Console.Write("Введите значение x: ");
 
         double x = double.Parse(Console.ReadLine());
 
-        double result = CalculateFunction(x);
+        double result = y(x);
 
         Console.WriteLine($"f({x}) = {result}");
     }
 
-    static double CalculateFunction(double x)
+    static double y(double x)
     {
         double sinx = Math.Sin(x);
         double sin2x = Math.Sin(2 * x);
         double sin3x = Math.Sin(3 * x);
         double sin4x = Math.Sin(4 * x);
 
-        double expression = sinx - sin2x - sin3x - sin4x;
+        double a = sinx - sin2x - sin3x - sin4x;
 
-        if (expression < 0)
-        {
-            Console.WriteLine("Внимание: выражение под корнем отрицательное!!");
-            return double.NaN;
-        }
-
-        double result = Math.Sqrt(expression);
+        double result = Math.Sqrt(a);
 
         return result;
     }
