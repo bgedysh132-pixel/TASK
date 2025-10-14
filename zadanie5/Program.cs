68414db531be44387eb0f0ec1583dba03ed19fc6
Boris Gedysh <Boris Gedysh@DESKTOP-IF9AAV7>
2025-10-14 21:24:19 +0500

    task05

---
+++ zadanie5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static double y(double a, double b)
+    {
+        return Math.Tan((1 + Math.Sqrt(a)) / b);
+    }
+
+    static void Main()
+    {
+        double x = y(3, 2) * y(2, 4) * y(7, 5);
+
+        Console.WriteLine($"x = {x:F3}");
+    }
+}
