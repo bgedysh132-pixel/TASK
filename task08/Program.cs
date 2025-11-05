f7584202509d5fc68f1920ba71fa056dce2069a8
Туйков Никита <bgedysh132@gmail.com>
2025-11-05 19:08:52 +0500

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Логическое выражение");
+
+        for (int A = 0; A <= 1; A++)
+        {
+            for (int B = 0; B <= 1; B++)
+            {
+                for (int C = 0; C <= 1; C++)
+                {
+                    bool result = YourLogicalExpression(A, B, C);
+
+                    Console.WriteLine($"A={A}, B={B}, C={C} -> Результат = {result}");
+                }
+            }
+        }
+    }
+
+    static bool YourLogicalExpression(int A, int B, int C)
+    {
+
+        bool expression = (A == 1 && B == 1) || (A == 0 && C == 1);
+        return expression;
+    }
+}
