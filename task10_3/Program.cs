ec17313fa61c15d356dcdac4c26d72eabef5b5cc
Никита Туйков <bgedysh132@gmail.com>
2025-12-03 23:23:55 +0500

    task10_3

---
+++ task10_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a (1 < a < 1.25):");
+            double a = double.Parse(Console.ReadLine());
+
+            int n = 0;
+            double sum = 0.0;
+            double term = 1.0;       
+
+            while (sum <= a)        
+            {
+                sum += term;
+                n++;
+                term /= 5.0;       
+            }
+
+            Console.WriteLine($"Наименьшая сумма S_n > a равна {sum}, при n = {n}");
+        }
+    }
+}
