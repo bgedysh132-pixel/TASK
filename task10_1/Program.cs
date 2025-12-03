23bffcb3c2565d956510291efbf7f97696bb1c36
Никита Туйков <bgedysh132@gmail.com>
2025-12-03 20:50:32 +0500

    task10_1

---
+++ task10_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskPowerTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double kWPerHp = 1.35962;
+
+            Console.WriteLine("Л.с.\tКВт");
+
+            for (int hp = 50; hp <= 250; hp += 5)
+            {
+                double kW = hp * kWPerHp;
+                Console.WriteLine($"{hp}\t{kW:F4}");
+            }
+        }
+    }
+}
+
